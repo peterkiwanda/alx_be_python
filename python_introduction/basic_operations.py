af567e48f54ec87521af07658c76e69c8c5866a9
peter kiwanda <kiwandapeter32@gmail.com>
2024-08-03 18:36:04 +0300

    python code performing basic arithmetic functions

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Assign values to the variables
 number1 = 10
 number2 = 5
@@ -8,4 +9,4 @@ mulltiplication = number1 * number2
 # Print the results in a human-readable format
 print(f"the addition of {number1} and {number2} is: {addition}")
 print(f"the subtraction of {number1} from {number2} is: {subtraction}")
-print(f"the multiplication od {number1} and {number2} is {multiplicationi}"
+print(f"the multiplication od {number1} and {number2} is {multiplication}")
