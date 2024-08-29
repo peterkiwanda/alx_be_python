83a9503f5dda90599eaa33172b10784b8c1f3c79
peter kiwanda <kiwandapeter32@gmail.com>
2024-08-29 14:48:55 +0300

    simple bank account class python code

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,26 @@
+# bank_account.py
+
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self._account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self._account_balance += amount
+        else:
+            print("Deposit amount must be positive.")
+
+    def withdraw(self, amount):
+        if amount > 0:
+            if self._account_balance >= amount:
+                self._account_balance -= amount
+                return True
+            else:
+                return False
+        else:
+            print("Withdrawal amount must be positive.")
+            return False
+
+    def display_balance(self):
+        print(f"Current Balance: ${self._account_balance:.2f}")
+
