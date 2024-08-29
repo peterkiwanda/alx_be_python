4dcbcdb3ef16826878620288a0cde1d017c36f39
peter kiwanda <kiwandapeter32@gmail.com>
2024-08-29 07:07:25 +0300

    temperature conversion tool python code

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -3,6 +3,7 @@
 # Define global conversion factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FREEZING_POINT_FAHRENHEIT = 32
 
 def celsius_to_fahrenheit(celsius):
     return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
